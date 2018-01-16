a6e1bfdc1c6331707ccf6701c313497a2d99673c
Xinze-hua <534863405@.com>
2018-01-16 15:29:38 +0800

    2018.1.16 (15.29)

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -9,7 +9,17 @@ def creatDataSet():
 
 def classfy0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]  # 获取数据的个数，shape()函数用来获取数组维度
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat**2
-    sqDistance = sqDiffMat.sum(axis = 0)
-print "huaxinze"
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet # 计算各个点与目标点在每个维度上的差值
+    sqDiffMat = diffMat**2  # 计算差值的平方
+    sqDistance = sqDiffMat.sum(axis = 1)   # 将每个点在各个维度上与目标点的差值平方相加
+    distance = sqDistance**0.5  # 进行开方处理，11,12,13,14,15是用来计算训练点与目标点的欧式距离
+    sortedDistIndicies = distance.argsort() # 按距离从小大进行排序，并记录各个点排序前的索引值
+    classCount = {}
+    for i in range(k):
+        votelabel = labels[sortedDistIndicies[i]]
+        classCount[votelabel] = classCount.get(votelabel, 0) + 1  # 相同的类别进行累加
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
+Group, Labels = creatDataSet()
+print classfy0([1.3, 1.4], Group, Labels, 3)
+
