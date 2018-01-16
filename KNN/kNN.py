bad64b810f68989dace7ce052a108b94727c0900
Xinze-Hua <534863405@qq.com>
2018-01-16 12:54:30 +0800

    2018.1.16(12:54)

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -1,4 +1,6 @@
+# -*- coding:utf-8 -*-
 from numpy import *
+import numpy
 import operator
 def creatDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -6,5 +8,7 @@ def creatDataSet():
     return group, labels
 
 def classfy0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-
+    dataSetSize = dataSet.shape[0]  # 获取数据的个数，shape()函数用来获取数组维度
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistance = sqDiffMat.sum(axis = 0)
