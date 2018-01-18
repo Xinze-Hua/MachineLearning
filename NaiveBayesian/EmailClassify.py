5edc2251b985721e6f808aabeea841137291a6a1
Xinze-Hua <534863405@qq.com>
2018-01-18 22:52:28 +0800

    2018.1.18 (22:52)

--- NaiveBayesian/EmailClassify.py
+++ NaiveBayesian/EmailClassify.py
@@ -16,6 +16,7 @@ def loadDataSet():
     classVec = [0, 1, 0, 1, 0, 1]
     return postingList, classVec
 
+
 # 建立词汇表
 def creatVocabList(dataList):
     Vocabset = set([])
@@ -61,9 +62,12 @@ def trainNaiveBayesian(trainMartix, trainCategory):
 def ClassifyNaiveBayesian(vecToClassify, p0vec, p1vec, pClass):
     p1 = sum(vecToClassify*p1vec) + log(pClass)
     p0 = sum(vecToClassify * p0vec) + log(1-pClass)
-    if(p0>p1):
-        print "正常邮件"
-    else: print "垃圾邮件"
+    if(p0<p1):
+        # print "正常邮件"
+        return 0
+    else:
+        # print "垃圾邮件"
+        return 1
 
 
 # 样本邮件单词提取
@@ -78,19 +82,52 @@ def SpiltText(fileList):
         for wordLine in wordList:
             listOfToken = regEx.split(wordLine)
             listOfToken = filter(lambda x: len(x) >= 3 and (not x.isdigit()) , listOfToken)
+            listOfToken = [el.lower() for el in listOfToken]
             Word_afetrDeal += listOfToken
         WordList.append(Word_afetrDeal)
     return WordList
 
-# 获得词汇表
-hamemailList = ['email/ham/' + el for el in listdir('email/ham')]
-SpamemailList = ['email/spam/' + el for el in listdir('email/spam')]
 
-hamWord = SpiltText(HamemailList)
-spamWordSet = SpiltText(SpamemailList)
-# VocubSet = spamWordSet|hamWordSet
-# VocubSet = [el.lower() for el in VocubSet]
+def ClassifyEmail():
+    HamemailList = ['email/ham/' + el for el in listdir('email/ham')]  # 正常邮件的文件名列表
+    SpamemailList = ['email/spam/' + el for el in listdir('email/spam')]  # 垃圾邮件的文件名列表
+    HamWordList = SpiltText(HamemailList)  #  提取所有正常邮件的单词列表
+    SpamWordList = SpiltText(SpamemailList)  #  提取所有垃圾邮件的单词列表
+    HamEmailClassVec = zeros(len(HamWordList))  # 正常邮件的类别向量
+    SpamEmailClassVec = ones(len(SpamWordList))  # 垃圾邮件的类别向量
+    WordList = SpamWordList + HamWordList  # 所有邮件的单词列表
+    EmailClassVec = hstack((HamEmailClassVec, SpamEmailClassVec))  # 邮件类别向量
+    EmailClassList = EmailClassVec.tolist()  # 所有邮件的类别列表
+    VocabList = creatVocabList(WordList)  # 建立词汇表
+    testVec = []  # 测试邮件的单词向量
+    trainMartix = []  # 训练集
+    testMartix = []  # 测试集
+    testClassVec = []  # 测试集的类别
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(WordList)))  # 在0-50之间产生10个随机数
+        testVec.append(WordList[randIndex])  # 获取10个随机测试邮件
+        testClassVec.append(EmailClassList[randIndex])  # 10个测试邮件的类别向量
+        del(WordList[randIndex])  # 获取40个训练邮件
+        del(EmailClassList[randIndex])  # 40个训练集的类别列表
+    for el in WordList:
+        trainMartix.append(setOfWordToVector(VocabList, el))  # 将训练集转化成数字向量形式
+    for el in testVec:
+        testMartix.append(setOfWordToVector(VocabList, el))   # 将测试集转化成数字向量形式
+    p0vec, p1vec, pClass = trainNaiveBayesian(trainMartix, EmailClassList)   # 计算先验概率，条件概率等
+    errorCount = 0.0
+    for i in range(10):
+        if(ClassifyNaiveBayesian(testMartix[i], p0vec, p1vec, pClass)==testClassVec[i]):
+            errorCount += 1
+    return errorCount/10.0
 
+def ClassifyTest():
+    sum = 0.0
+    for i in range(10):
+        p = ClassifyEmail()
+        sum += p
+    print sum/10.0
+
+ClassifyTest()
 
 
 
