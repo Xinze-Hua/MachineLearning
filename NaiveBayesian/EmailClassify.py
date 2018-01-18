fdee3ef3b29cd0b58f122236399465dd88af497b
Xinze-Hua <534863405@qq.com>
2018-01-18 19:05:37 +0800

    2018.1.18 (19:05)

--- NaiveBayesian/EmailClassify.py
+++ NaiveBayesian/EmailClassify.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
 from numpy import *
+from os import listdir
 import re  #  正则表达式
 
-def loadDataSet():  # 训练实例
+# 训练实例
+def loadDataSet():
     postingList = [
         ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
         ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -14,23 +16,25 @@ def loadDataSet():  # 训练实例
     classVec = [0, 1, 0, 1, 0, 1]
     return postingList, classVec
 
-
-def creatVocabList(dataSet):  # 建立字符表
+# 建立词汇表
+def creatVocabList(dataList):
     Vocabset = set([])
-    for doucument in dataSet:
+    for doucument in dataList:
         Vocabset = Vocabset | set(doucument)
     return list(Vocabset)
 
 
-def setOfWordToVector(vocabList, inputSet):  # 将实例通过字符表转化为向量
+# 将实例通过字符表转化为向量
+def setOfWordToVector(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
-            returnVec[vocabList.index(word)] += 1
+            returnVec[vocabList.index(word)] += 1  #  这里使用的是词袋模型
         else: print "the word ", word, "is not in my vocabulary!"
     return returnVec
 
 
+# 概率训练
 def trainNaiveBayesian(trainMartix, trainCategory):
     docNum = len(trainCategory)
     numTrainMatrix = len(trainMartix)
@@ -53,6 +57,7 @@ def trainNaiveBayesian(trainMartix, trainCategory):
     return p0Vect, p1Vect, pAbusive
 
 
+# 贝叶斯分类器
 def ClassifyNaiveBayesian(vecToClassify, p0vec, p1vec, pClass):
     p1 = sum(vecToClassify*p1vec) + log(pClass)
     p0 = sum(vecToClassify * p0vec) + log(1-pClass)
@@ -61,15 +66,60 @@ def ClassifyNaiveBayesian(vecToClassify, p0vec, p1vec, pClass):
     else: print "垃圾邮件"
 
 
-ListofPost, ListClasses = loadDataSet()  #  获得实例数据
-VocabList = creatVocabList(ListofPost)   #  根据实例数据生成字典
-trainMat = []
-for postinDoc in ListofPost:
-    trainMat.append(setOfWordToVector(VocabList, postinDoc))
-p0Vect, p1Vect, pClass = trainNaiveBayesian(trainMat, ListClasses)
-temp = setOfWordToVector(VocabList, ['stupid', 'love', 'dalmation'])
-ClassifyNaiveBayesian(temp, p0Vect, p1Vect, pClass)
+# 样本邮件单词提取
+def SpiltText(fileList):
+    regEx = re.compile('\\W*')
+    WordList = []
+    i = 0
+    for filename in fileList:
+        fr= open(filename)
+        wordList = fr.readlines()
+        Word_afetrDeal = []
+        for wordLine in wordList:
+            listOfToken = regEx.split(wordLine)
+            listOfToken = filter(lambda x: len(x) >= 3 and (not x.isdigit()) , listOfToken)
+            Word_afetrDeal += listOfToken
+        WordList.append(Word_afetrDeal)
+    return WordList
 
+# 获得词汇表
+hamemailList = ['email/ham/' + el for el in listdir('email/ham')]
+SpamemailList = ['email/spam/' + el for el in listdir('email/spam')]
+
+hamWord = SpiltText(HamemailList)
+spamWordSet = SpiltText(SpamemailList)
+# VocubSet = spamWordSet|hamWordSet
+# VocubSet = [el.lower() for el in VocubSet]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ListofPost, ListClasses = loadDataSet()  #  获得实例数据
+# VocabList = creatVocabList(ListofPost)   #  根据实例数据生成字典
+# trainMat = []
+# for postinDoc in ListofPost:
+#     trainMat.append(setOfWordToVector(VocabList, postinDoc))
+# p0Vect, p1Vect, pClass = trainNaiveBayesian(trainMat, ListClasses)
+# temp = setOfWordToVector(VocabList, ['stupid', 'love', 'dalmation'])
+# ClassifyNaiveBayesian(temp, p0Vect, p1Vect, pClass)
+#
 # def SplitEmailtext(filename):
 #     regEx = re.compile('\\W*')
 #     fr = open(filename)
