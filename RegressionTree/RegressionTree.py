ce73d2d58cfb8f72113216776ebeaa002ba18565
Xinze-hua <534863405@.com>
2018-01-27 22:54:20 +0800

    2018.1.27 (22:54)

--- RegressionTree/RegressionTree.py
+++ RegressionTree/RegressionTree.py
@@ -41,6 +41,7 @@ def SplitDataSet(DataSet, feature, value):
 
 #  获取样本特征值的矩阵
 def ObtainFeatureValueMat(DataSet):
+    '''This function is to obtain the matrix of feature value!'''
     dataSet = np.array(DataSet)
     featureNum = len(FEATURE) # 获取样本所有特征数目
     featureMat = []
@@ -53,7 +54,7 @@ def ObtainFeatureValueMat(DataSet):
 
 ####################################################### 回归树生成部分 ###################################################
 
-#  数据集误差计算函数 -- 计算数据集的方差并乘以数据集的个数
+#  数据集误差计算函数
 def ComputeVar(DataSet):
     '''This function is to compute the error of data set!'''
     dataSet = np.array(DataSet)
@@ -62,12 +63,13 @@ def ComputeVar(DataSet):
     else: return 0.0
 
 
-# 叶子节点生成函数 -- 计算数据集均值
+#  叶子节点生成函数
 def regLeaf(DataSet):
+    '''This function is to get leaf node!!!'''
     return np.mean(DataSet[:,-1])
 
 
-#  选择最优特征以及所对应的特征值 -- 参数：数据集，特征值集，叶节点形式（均值or其他），误差计算方式，用户自定义参数
+#  选择最优特征以及所对应的特征值
 def ChooseBestFeatureAndValue(DataSet, featureMat, leafType = regLeaf, errType = ComputeVar, ops=(1,4)):
     '''This function is to choose the best Splited feature and the value!'''
     dataSet = np.array(DataSet)
@@ -92,7 +94,7 @@ def ChooseBestFeatureAndValue(DataSet, featureMat, leafType = regLeaf, errType =
         if BestS > min(DataErrorVec): BestS = min(DataErrorVec)
     BestFeatureAndValue = sorted(BestFeatureAndValue.iteritems(), key = operator.itemgetter(1), reverse = True)[0]
     # 如果分割后的数据集误差与不分割的数据集误差在很小，则不需要继续分割
-    if (S - BestS)<tolS:
+    if (S - BestS) < tolS:
         return None, leafType(dataSet)
     # 如果切割后的数据集规模过于小，则停止分割
     LeftDataSet, RightDataSet = SplitDataSet(DataSet, BestFeatureAndValue[0], BestFeatureAndValue[1]) # 按照最优特征及其对应的值进行分割数据得到子集
@@ -102,8 +104,9 @@ def ChooseBestFeatureAndValue(DataSet, featureMat, leafType = regLeaf, errType =
     return BestFeatureAndValue[0], BestFeatureAndValue[1]
 
 
-# 递归构造回归树
+#  递归构造回归树函数
 def createTree(DataSet, featureMat, leafType = regLeaf, errType = ComputeVar, ops = (1,4)):
+    '''This function is to built regression tree!'''
     BestFeature, BestValue = ChooseBestFeatureAndValue(DataSet, featureMat, leafType, errType, ops)
     if BestFeature == None:  # 如果无特征返回，表明数据集分割停止，应当生成叶子节点
         return BestValue
@@ -115,10 +118,56 @@ def createTree(DataSet, featureMat, leafType = regLeaf, errType = ComputeVar, op
     retTree['right'] = createTree(RightDataSet, featureMat, leafType, errType, ops)
     return retTree
 
-# DataSet = LoadDataSet('ex0.txt')
-# dataSet =[]
-# for Line in DataSet:
-#     Line = Line[1:]
-#     dataSet.append(Line)
-# featureMat = ObtainFeatureValueMat(dataSet)
-# print createTree(dataSet, featureMat)
+
+#  生成回归树
+DataSet = LoadDataSet('ex2.txt')
+featureMat = ObtainFeatureValueMat(DataSet)
+RegressionTree = createTree(DataSet, featureMat, ops = (100, 1))
+print RegressionTree
+
+####################################################### 回归树剪枝部分 ###################################################
+
+#  判断对象是叶子节点还是子树
+def isTree(obj):
+    return type(obj).__name__ == 'dict'
+
+
+#  计算树的均值
+def getMean(tree):
+    if isTree(tree['right']): tree['right'] = getMean(tree['right'])
+    if isTree(tree['left']): tree['left'] = getMean(tree['left'])
+    return (tree['right'] + tree['left']) / 2.0
+
+
+#  计算测试集误差
+def ComputeTestDataError(TestDataSet, value):
+    '''This function is to compute the error of test data set!'''
+    testDataSet = np.array(TestDataSet)
+    Len = len(testDataSet)
+    if Len > 0: return np.var(testDataSet[:,-1] - value) * Len
+    else: return 0.0
+
+
+#  对CART树进行剪枝
+def prune(tree, testData):
+    if len(testData) == 0: # 递归终止条件1：# 如果该子树对应的测试集数据出现残缺，则直接返回子树的平均值作为新的节点
+        return getMean(tree)
+    if (isTree(tree['right']) or isTree(tree['left'])):  #  如果树的左节点或右节点为子树，则进行数据切割和递归剪枝处理
+        leftTestData, rightTestData = SplitDataSet(testData, tree['spInd'], tree['spVal'])
+        if isTree(tree['left']): tree['left'] = prune(tree['left'], leftTestData)  #  如果左节点为子树，则进行树剪枝处理
+        if isTree(tree['right']): tree['right'] = prune(tree['right'], rightTestData)  #  如果右节点为子树，则进行树剪枝处理
+    if not isTree(tree['right']) and not isTree(tree['left']):  # 递归终止条件2：直到左右子节点均不是子树时
+        leftTestData, rightTestData = SplitDataSet(testData, tree['spInd'], tree['spVal'])
+        treeMean = (tree['left'] + tree['right']) / 2.0
+        errorNoMerge = ComputeTestDataError(leftTestData, tree['left']) + ComputeTestDataError(rightTestData, tree['right'])
+        errorMerge = ComputeTestDataError(testData, treeMean)
+        if errorMerge <= errorNoMerge:
+            return treeMean
+        else:
+            return tree
+    else:   # 如果当前子树的左右子节点中有一个是树，另一个是节点，则直接返回子树
+        return tree
+
+
+TestDataSet = LoadDataSet('ex2test.txt')
+print prune(RegressionTree, TestDataSet)
